90aa1355d4e42d6a75b37b15595967255d66290c
ayase-mstk <masa1215.hayase@gmail.com>
2023-11-27 22:25:39 +0900

    chat server using pthread with one port

--- server.cpp
+++ server.cpp
@@ -6,55 +6,123 @@
 #include <errno.h>
 #include <cstring>
 #include <iostream>
+#include <pthread.h>
 
-int	main()
+#define BUF_SIZE 100
+#define MAX_CLNT 256
+
+int clnt_cnt=0;
+int clnt_socks[MAX_CLNT];
+pthread_mutex_t mutex;
+
+void * handle_clnt(void * arg);
+void send_msg(char * msg, int len);
+
+int	main(int ac, char *av[])
 {
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1)
+	int	serv_sock, clnt_sock;
+	struct sockaddr_in	serv_addr, clnt_addr;
+	socklen_t	clnt_addr_size;
+	pthread_t	t_id;
+
+	if(ac != 2) {
+		printf("Usage : %s <port>\n", av[0]);
+		exit(1);
+	}
+
+	pthread_mutex_init(&mutex, NULL);
+	serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (serv_sock == -1)
 	{
 		std::cout << "Error: socket:" << std::strerror(errno);
 		exit (1);
 	}
-	
-	struct sockaddr_in addr;
-	memset(&addr, 0, sizeof(struct sockaddr_in));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(1234); // network byte order (big endian) に変換
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	// inet_aton("127.0.0.1", &addr.sin_addr);
+
+	memset(&serv_addr, 0, sizeof(struct sockaddr_in));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(atoi(av[1]));
+	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	// ソケット登録
-	bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+	if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+	{
+		std::cout << "Error: bind:" << std::strerror(errno);
+		close(serv_sock);
+		exit (1);
+	}
 
 	// 受信
-	if (listen(sockfd, SOMAXCONN) < 0)
+	if (listen(serv_sock, SOMAXCONN) < 0)
 	{
 		std::cout << "Error listen:" << std::strerror(errno);
-		close(sockfd);
+		close(serv_sock);
 		exit(1);
 	}
 
-	struct sockaddr_in	get_addr;
-	socklen_t	len = sizeof(struct sockaddr_in);
-	int	connect = accept(sockfd, (struct sockaddr *)&get_addr, &len);
+	while (1)
+	{
+		clnt_addr_size = sizeof(struct sockaddr_in);
+		clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
 
-	if (connect < 0)
+		pthread_mutex_lock(&mutex);
+		clnt_socks[clnt_cnt++] = clnt_sock;
+		pthread_mutex_unlock(&mutex);
+
+		pthread_create(&t_id, NULL, handle_clnt, (void *)&clnt_sock);
+		pthread_detach(t_id);
+		printf("Connected clinet IP: %s \n", inet_ntoa(clnt_addr.sin_addr));
+	}
+	close(serv_sock);
+	return 0;
+}
+
+void	*handle_clnt(void *arg)
+{
+	int	clnt_sock = *((int *)arg);
+	int	msg_len = 0;
+	char msg[BUF_SIZE];
+
+	sleep(5);
+
+	if (clnt_sock < 0)
 	{
 		std::cout << "Error accept:" << std::strerror(errno);
 		exit(1);
 	}
 
-	// 受信
-	char str[12];
-	recv (connect, str, 12, 0);
-	std::cout << "accept: " << str << std::endl;
+	while ((msg_len = recv(clnt_sock, msg, sizeof(msg), 0)) != 0)
+	{
+		if (msg_len == -1)
+		{
+			std::cout << "Error recv:" << std::strerror(errno);
+			exit(1);
+		}
+		send_msg(msg, msg_len);
+	}
+	
+	pthread_mutex_lock(&mutex);
+	for (int i = 0; i < clnt_cnt; i++)
+	{
+		if (clnt_sock == clnt_socks[i])
+		{
+			while (i++ < clnt_cnt - 1)
+				clnt_socks[i] = clnt_socks[i + 1];
+			break ;
+		}
+	}
 
-	// 再送信
-	send(connect, str, 12, 0);
-	std::cout << "send: " << str << std::endl;
+	clnt_cnt--;
+	pthread_mutex_unlock(&mutex);
+	close(clnt_sock);
+	return (NULL);
+}
 
-	close(connect);
-	close(sockfd);
-
-	return (0);	
+void	send_msg(char *msg, int	len)
+{
+	pthread_mutex_lock(&mutex);
+	for (int i = 0; i < clnt_cnt; i++)
+	{
+		send(clnt_socks[i], msg, len, 0);
+	}
+	pthread_mutex_unlock(&mutex);
 }
